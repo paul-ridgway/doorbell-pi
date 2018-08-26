213a33c38aca0583a49ce137f9e94bae2880a8ea
Paul Ridgway <paul@ridgway.io>
2018-08-26 09:20:11 +0100

    Path handling

--- doorbell/webserver.py
+++ doorbell/webserver.py
@@ -1,11 +1,14 @@
+import os
+import inspect
+from os.path import dirname, abspath
+
 from flask import (
     Flask,
     render_template
 )
 
 # Create the application instance
-app = Flask(__name__, root_path="../web")
-
+app = Flask(__name__, root_path=os.path.join(dirname(dirname(abspath(__file__))), "web"))
 
 # Create a URL route in our application for "/"
 @app.route('/')
