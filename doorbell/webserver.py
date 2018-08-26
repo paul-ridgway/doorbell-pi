8fdf3d5a2bf278cd1ccd9fdcbf2ac9e11977e492
Paul Ridgway <paul@thefloow.com>
2018-08-26 09:06:06 +0100

    Web paths etc

--- doorbell/webserver.py
+++ doorbell/webserver.py
@@ -4,18 +4,14 @@ from flask import (
 )
 
 # Create the application instance
-app = Flask(__name__, template_folder="../web")
+app = Flask(__name__, root_path="../web")
+
 
 # Create a URL route in our application for "/"
 @app.route('/')
 def home():
-    """
-    This function just responds to the browser ULR
-    localhost:5000/
-
-    :return:        the rendered template 'home.html'
-    """
     return render_template('index.html')
+
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
